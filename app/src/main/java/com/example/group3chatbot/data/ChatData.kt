d2bb43e7ea35b555e50ab64c9c9d7ebbeb7ea5af
Shubham Misra <121442461+Sam-Orion@users.noreply.github.com>
2024-04-21 02:45:36 +0530

    Edit ChatData.kt

--- app/src/main/java/com/example/group3chatbot/data/ChatData.kt
+++ app/src/main/java/com/example/group3chatbot/data/ChatData.kt
@@ -4,4 +4,62 @@ object ChatData {
 
     val api_key = System.getenv("API_KEY")
 
+    suspend fun getResponse(prompt: String): Chat {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro", apiKey= api_key
+        )
+
+        try {
+            val response = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(prompt)
+            }
+
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+
+        } catch (e: ResponseStoppedException) {
+            return Chat(
+                prompt = e.message ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+
+    }
+
+    suspend fun getResponse(prompt: String, bitmap: Bitmap): Chat {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro-vision", apiKey= api_key
+        )
+
+        try {
+
+            val inputContent = content {
+                image(bitmap)
+                text(prompt)
+            }
+
+            val response = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(inputContent)
+            }
+
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+
+        } catch (e: ResponseStoppedException) {
+            return Chat(
+                prompt = e.message ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+
+    }
+
 }
