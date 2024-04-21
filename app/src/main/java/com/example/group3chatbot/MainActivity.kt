ebd88f16a67ec78c5c3341028292c8376516314b
Shubham Misra <121442461+Sam-Orion@users.noreply.github.com>
2024-04-21 12:48:55 +0530

    Edit MainActivity.kt

--- app/src/main/java/com/example/group3chatbot/MainActivity.kt
+++ app/src/main/java/com/example/group3chatbot/MainActivity.kt
@@ -1,43 +1,104 @@
 package com.example.group3chatbot
 
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.group3chatbot.ui.theme.Group3ChatBotTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class MainActivity : ComponentActivity() {
+
+    private val uriState = MutableStateFlow("")
+
+    private val imagePicker =
+        registerForActivityResult<PickVisualMediaRequest, Uri>(
+            ActivityResultContracts.PickVisualMedia()
+        ) { uri ->
+            uri?.let {
+                uriState.update { uri.toString() }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Group3ChatBotTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background)
+                {
+                    Scaffold(
+                        topBar = {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(MaterialTheme.colorScheme.primary)
+                                    .height(55,dp)
+                                    padding(horizontal = 16.dp)
+                            )
+                            {
+                                Text(
+                                    modifier = Modifier.align(Alignment.CenterStart),
+                                    text = stringResource(id = R.string.app_name),
+                                    fontSize = 19.sp,
+                                    color = MaterialTheme.colorScheme.onPrimary
+                                )
+                            }
+                        }
+                    ) {
+                        ChatScreen(paddingValues = it)
+                    }
                 }
             }
         }
     }
-}
+    
+    @Composable
+    fun ChatScreen(paddingValues: PaddingValues) {
+        val chatViewModel = viewModel<ChatViewModel>()
+        val chatState = chatViewModel.chatState.collectAsState(). value
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(top = paddingValues.calculateToppadding()),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                reverseLayout = true
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Group3ChatBotTheme {
-        Greeting("Android")
+            ) {
+
+            }
+        }
     }
+    
 }
+
