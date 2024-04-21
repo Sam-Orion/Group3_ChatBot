490c50a76f2416d42e389dd9baf72111a2fb9a51
Shubham Misra <121442461+Sam-Orion@users.noreply.github.com>
2024-04-21 12:48:25 +0530

    Edit ChatState.kt

--- app/src/main/java/com/example/group3chatbot/ChatState.kt
+++ app/src/main/java/com/example/group3chatbot/ChatState.kt
@@ -1,9 +1,9 @@
 package com.example.group3chatbot
 
 import android.graphics.Bitmap
+import com.example.group3chatbot.data.Chat
 
-data class ChatState {
-    val chatlist: MutableList<Chat> = mutableListOf(),
-    val prompt: String = "",
+data class ChatState (val chatList: MutableList<Chat> = mutableListOf()) {
+    val prompt: String = ""
     val bitmap: Bitmap? = null
 }
