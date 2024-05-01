069c3e714b2c4b6cc323cca22ba91b65bd7fc942
Shubham Misra <121442461+Sam-Orion@users.noreply.github.com>
2024-05-01 17:28:11 +0530

    Edit ChatUIEvent.kt

--- app/src/main/java/com/example/group3chatbot/ChatUIEvent.kt
+++ app/src/main/java/com/example/group3chatbot/ChatUIEvent.kt
@@ -2,6 +2,9 @@ package com.example.group3chatbot
 
 import android.graphics.Bitmap
 
+/**
+ * @author Shubham Misra
+ */
 sealed class ChatUIEvent {
     data class UpdatePrompt(val newPrompt: String): ChatUIEvent()
     data class SendPrompt(
